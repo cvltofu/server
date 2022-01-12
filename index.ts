2cc6184813ac9d6ebb78391bb6c79169dafd31a3
Ilya <kexit353@gmail.com>
2022-01-12 12:58:47 +0300

    DB connect, server deps

---
+++ index.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 6000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.options('*', cors());
+
+async function startApp() {
+  try {
+    await mongoose.connect(
+      process.env.DB_URL
+      //   {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+      // }
+    );
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startApp();
