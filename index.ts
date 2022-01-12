c710408360cb823ae0487d9906b793fd831dcc29
Ilya <kexit353@gmail.com>
2022-01-12 14:07:56 +0300

    add models

--- index.ts
+++ index.ts
@@ -17,10 +17,10 @@ async function startApp() {
   try {
     await mongoose.connect(
       process.env.DB_URL
-      //   {
+      //  {
       //   useNewUrlParser: true,
       //   useUnifiedTopology: true,
-      // }
+      //  }
     );
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`);
---
+++ models/todo-model.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const TodoSchema = new mongoose.Schema({
+    date: { type: Date, required: true },
+    title: { type: String, required: true },
+    task: { type: String, required: true },
+});
+
+export default mongoose.model('Todo', TodoSchema);
