9fa7833274ad1026cb0615389775e299115d0861
Ilya <kexit353@gmail.com>
2022-01-13 11:14:04 +0300

    install jwt, add tokens logic

--- services/user-service.ts
+++ services/user-service.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import userModel from '../models/user-model';
 import mailService from './mail-service';
 import UserDto from '../dtos/user-dto';
+import tokenService from './token-service';
 
 class UserService {
   async registration(email: string, password: string) {
