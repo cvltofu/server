6014bddf86db1cfcbf3638e1e629c440c714325d
Ilya <kexit353@gmail.com>
2022-01-13 11:41:13 +0300

    add error-handler

--- exceptions/api-error.ts
+++ exceptions/api-error.ts
@@ -1,19 +1,19 @@
 export default class ApiError extends Error {
-  status: number;
+  status;
 
-  errors: [];
+  errors;
 
-  constructor(status: number, message: string, errors: [] = []) {
+  constructor(status, message, errors = []) {
     super(message);
     this.status = status;
     this.errors = errors;
   }
 
   static UnauthorizedError() {
-    return new ApiError(401, 'Пользователь не авторизован');
+    return new ApiError(401, 'The user is not logged in.');
   }
 
-  static BadRequest(message: string, errors: [] = []) {
+  static BadRequest(message, errors = []) {
     return new ApiError(400, message, errors);
   }
 }
