f66e9707ef1af4f5de028ddf33dd8d34deb75ece
Ilya <kexit353@gmail.com>
2022-01-13 11:03:39 +0300

    Add nodemailer, email activation

--- services/mail-service.ts
+++ services/mail-service.ts
@@ -1,5 +1,34 @@
+import nodemailer from 'nodemailer';
+
 class MailService {
-  async sendActivationMail() {}
+  transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.yandex.ru',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'i.shokel.vironit@vironit.ru',
+        pass: 'lmddisdkpvjobqoi',
+      },
+    });
+  }
+
+  async sendActivationMail(to: string, link: string) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: `Активация аккаунта на ${process.env.API_URL}`,
+      text: '',
+      html: `
+                    <div>
+                        <h1>Для активации перейдите по ссылке</h1>
+                        <a href="${link}">${link}</a>
+                    </div>
+                `,
+    });
+  }
 }
 
 export default new MailService();
