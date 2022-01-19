aa6055d8cbdd05a14d09ff6235186fa738f59a19
Ilya <kexit353@gmail.com>
2022-01-19 12:05:10 +0300

    upd todo-controller

--- controllers/todo-controller.ts
+++ controllers/todo-controller.ts
@@ -1,9 +1,36 @@
+import todosService from '../services/todos-service.js';
+
 class TodoController {
-  async getTodos() {}
+  async getTodos(req, res, next) {
+    try {
+      const todos = await todosService.getTodos();
 
-  async postTodos() {}
+      return res.json(todos);
+    } catch (e) {
+      next(e);
+    }
+  }
 
-  async deleteTodos() {}
+  async postTodos(req, res, next) {
+    try {
+      const { date, title, task } = req.body;
+      const todo = await todosService.postTodos(date, title, task);
+
+      return res.json(todo);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async deleteTodos(req, res, next) {
+    try {
+      const todo = await todosService.deleteTodos(req.params.id);
+
+      return res.json(todo);
+    } catch (e) {
+      next(e);
+    }
+  }
 }
 
 export default new TodoController();
